da9896568351a64bc2da75896c97baadfe63d79c
Saviam <saviambruno@gmail.com>
2025-02-14 10:34:37 -0300

    tsc com Tuple

--- code/index.ts
+++ code/index.ts
@@ -7,6 +7,10 @@
 
 //---- segundo dia (pelo curso)-----
 
-let values: number [] = [10, 20, 40, 30] //colocando chavinhas amarelhas depois de definir q é number, ele vai indicar se tem algum erro dentro do array
+// let values: number [] = [10, 20, 40, 30] //colocando chavinhas amarelhas depois de definir q é number, ele vai indicar se tem algum erro dentro do array
 
+
+//Apple, 3, true
+
+let item:[string, number, boolean] = ['Apple', 3, true] // aqui é o uso do Taple, onde voce consegue definir diferentes tipos dentro do mesmo array, só definir quais são e qual a ordem q eles devem vir
 
