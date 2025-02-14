01e7be300a6a39281cb7091eb55ec019c9ceb79e
Saviam <saviambruno@gmail.com>
2025-02-14 10:30:08 -0300

    tsc com array

--- code/index.ts
+++ code/index.ts
@@ -1,6 +1,12 @@
-function add (num1: number, num2: number){
-    return num1 + num2
-}
+// function add (num1: number, num2: number){
+//     return num1 + num2
+// }
 
-let result = add(5,8)
-console.log(result)
+// let result = add(5,8)
+// console.log(result)
+
+//---- segundo dia (pelo curso)-----
+
+let values: number [] = [10, 20, 40, 30] //colocando chavinhas amarelhas depois de definir q é number, ele vai indicar se tem algum erro dentro do array
+
+
