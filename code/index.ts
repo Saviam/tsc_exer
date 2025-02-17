ecc47543742082439045846b77f376405c3a5f41
Saviam <saviambruno@gmail.com>
2025-02-17 10:23:32 -0300

    inicio de exer

--- code/index.ts
+++ code/index.ts
@@ -235,13 +235,16 @@ permitido para ser inserido no input ou variavel */
 
 // Day 3 - Exercise 5
 
-const sequence :number[]  = Array.from(Array(10).keys());
-const animals : string[]  = ['pangolin', 'aardvark', 'echidna', 'binturong'];
-const stringsAndNumbers : (number | string)[] = [1, 'one', 2, 'two', 3, 'three'];
-const allMyArrays :(number | string)[][] = [sequence, animals, stringsAndNumbers];
-/* Na ultima linhas ele colocou chaves duas vezes porque é um array dos outros arrays, 
-então precisa indicar assim */
+// const sequence :number[]  = Array.from(Array(10).keys());
+// const animals : string[]  = ['pangolin', 'aardvark', 'echidna', 'binturong'];
+// const stringsAndNumbers : (number | string)[] = [1, 'one', 2, 'two', 3, 'three'];
+// const allMyArrays :(number | string)[][] = [sequence, animals, stringsAndNumbers];
+// /* Na ultima linhas ele colocou chaves duas vezes porque é um array dos outros arrays, 
+// então precisa indicar assim */
 
-console.log(allMyArrays);
+// console.log(allMyArrays);
+
+
+//----Dia 4 ( pelo curso)---------------
 
 
